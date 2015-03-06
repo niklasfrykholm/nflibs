c9475342e5bd96460e9f9203d688278788a85a85
Niklas Frykholm <niklas.frykholm@autodesk.com>
2015-03-06 07:55:43 -0800

    ConfigData documentation.

--- README.md
+++ README.md
@@ -1,17 +1,90 @@
 # nflibs
 
-This is a collection of minimalistic C libraries designed to work together.
+*nflibs* is a collection of  C99 utility libraries. Each library has a singular specific purpose and a minimalistic decoupled API. But unlike for example the  great `stb_*` libraries, they are not completely isolated units. Instead, they have been written to work together. For example, *nf_config_data* uses *nf_string_table* to store strings and *nf_json_parser* produces output in the *nf_config_data* format.
 
-It contains the following libraries:
+## Libraries
 
-### nf_config_data.c
+This section provides a short description of all the different libraries, see the `*.c` files for a detailed description of each library.
 
-A minimalistic representation of tree configuraiton data, suitable for storing a JSON file.
+### nf_config_data
 
-### nf_memory_tracker.c
+Manipulates *configuration data* objects. This is intended as a general way of representing arbitrary data and corresponds to data that can be stored in a JSON file (bools, numbers, strings, arrays and objects). The data is kept in a single memory block that can be saved to and loaded from disk without any need for pointer patching.
 
-Tracks memory allocations and stores them in a stream for later analysis.
+### nf_json_parser
 
-### nf_string_table.c
+Parses JSON files into *Config Data* objects. Can also parse JSON extensions, such as comments, python multline strings, etc, based on configuration settings.
 
-Implements a string table for string *interning*.
+### nf_memory_tracker
+
+Tracks memory allocations in a buffer that can be streamed out to disk or network for later analysis.
+
+### nf_string_table
+
+Implements string to symbol (integer) conversion (i.e. interning), for more compact representation of string data.
+
+## Design Philosophy
+
+The *nflibs* are an experiment in creating a set of libraries that interoperate (so that higher level functionality can be built on top of a lower level foundation), but still are decoupled and isolated enough that you can plug them into any kind of project without feeling too bad about it. 
+
+### C99
+
+The libraries are written in C99 rather than C++. There are a number of reasons for this:
+
+* Since C++ is so huge and everybody uses a different subset of it, making a reusable library that fits well into different C++ projects is difficult.
+* C99 has ABI compatibility, meaning that with FFI the libraries can be used from a number of different scripting languages.
+* C++ is a complex monster that nobody should be forced to learn.
+
+If you are using Visual Studio, you need 2013 to compile the libraries,
+because that's when Microsoft decided to add (partial) C99 support. Feel free to pester Microsoft about adding support for the `inline` keyword.
+
+### Single file interface
+
+The libraries are designed with a *single file* design philosophy. Each library is fully stored in a single `.c` file that contains the interface, the implementation and any unit tests. Basically, the idea is that you should be able to completely understand a library by looking at a single file. Also, that it should be easy to pick and choose just the libraries that you need.
+
+The most controversial choice is to not use any *header files*. Instead, each `.c` file includes the definitions for the external structs and functions that it needs to use. This is a deliberate choice to encourage a decoupled design and shorter compile times. But it is sort of an experiment, I might decide to change it later if it becomes to unwieldy.
+
+If you want header files you can create them with:
+
+    make header
+
+This creates a single merged `nflibs.h` header file.
+
+Unit tests are stored directly in the `.c` files. If you compile a library with `-DNFXX_UNIT_TEST` (where *XX* is the abbreviation for that library) it will create an executable that runs the unit tests. A successful test run produces no output.
+
+To compile and run all unit tests, use:
+
+    make run_all_tests
+
+This is also the default target, so you can type just `make` to do the same thing.
+
+### Memory allocations
+
+All libraries are written in a data-oriented style. They try to minimize memory allocations and lay the data out linearly in memory for improved cache performance.
+
+Most of the libraries allocate the data in a single memory block. The block starts with a header which is followed by bulk data. References within the memory block use offsets rather than allocators, so it can be moved freely in memory and reallocated with `realloc`.
+
+Typically the same data structure is used for both online and offline (disk) storage, so the data can be read directly from disk into memory and then be dynamically modified. Since dynamic data has other demands the data may be laid out in a slightly different (but still compatible) way. Typically, in these cases, a `pack()` function is offered that packs the data into the most efficient format for static use.
+
+The libraries never use `malloc()` or `free()` directly. Instead the user can pass an allocator function. The allocator function always use the format:
+
+```cpp
+typedef void *(*realloc_f)(void *ptr, int osize, int nsize, char *file, int line, char *tag);
+```
+
+Basically this is `realloc()`, but with some additional information (file name, line number, tag), that can be used for memory tracking.
+
+Note that the old size of the allocation is always passed in the `osize` parameter, because the libraries always keep track of the size of their allocations. You can use this to build a more efficient allocator, since you don't need to track the size of the memory blocks.
+
+### `int` vs `unsigned`
+
+`nflibs` use `int` everywhere except where it is explicitly manipulating bit flags. The extra bit you get from using `unsigned` is usually not worth the extra risk of unintentional underflow.
+
+For 32 bit values, `int` and `unsigned` are used to avoid having to include `stdint` everywhere. For other bit sizes, the bit size is always explicitly specified: `int16_t`, `uint64_t`, etc.
+
+### Documentation
+
+Functions are documented in the implementation rather than in the interface in order to keep the interface absolutely minimal. (Both to minimize compile times and to make it easy to get a quick overview of the interface.)
+
+The documentation uses Markdown syntax. You can extract the documentation into Markdown files by running:
+
+    make doc
--- nf_config_data.c
+++ nf_config_data.c
@@ -1,3 +1,9 @@
+// # Config data
+//
+// This library implements a dynamic generic data container that can hold
+// bools, numbers, strings, arrays and objects. Basically, anything you can
+// represent with a JSON file.
+
 // ## Interface
 
 struct nfcd_ConfigData;
@@ -58,12 +64,16 @@ int nfst_to_symbol(struct nfst_StringTable *st, const char *s);
 int nfst_to_symbol_const(const struct nfst_StringTable *st, const char *s);
 const char *nfst_to_string(struct nfst_StringTable *, int symbol);
 
-// nfcd_loc encodes type and offset into data
-// array:  [size allocated loc...]
-// object: [size allocated keys... values...]
-// dynamic arrays and objects grow by chaining, nfcd_loc to next section of array
-// each part o fthe chain doubls in size
+// All the data is stored in a single buffer. A data reference (`nfcd_loc`)
+// encodes the data type and the offset into this buffer in a single int.
+//
+// Strings are stored in an nfst_StringTable, so for strings the offset
+// represents the offset into the string table.
+//
+// TODO: Currently the strings are stored in a separately allocated pointer,
+// but we should change that.
 
+// Container for the config data.
 struct nfcd_ConfigData
 {
 	int allocated_bytes;
@@ -74,25 +84,43 @@ struct nfcd_ConfigData
 	void *realloc_user_data;
 };
 
-struct nfcd_Block
+// Header for array and object data. The data is stored in a chain of blocks.
+// When the data grows dynamically, we add bigger and bigger blocks to the
+// chain. When we pack the data for disk storage, these chais are coalesced
+// into a single block.
+//
+// For an array, the data stored in a block consists of the array items:
+//
+//     [item] [item] ...
+//
+// For an object, the data consists of interleaved keys and values:
+//
+//     [key] [vaulue] [key] [value] ...
+struct block
 {
 	int allocated_size;
 	int size;
 	nfcd_loc next_block;
 };
 
-struct nfcd_ObjectItem
+// Represents a stored item in an object block.
+struct object_item
 {
 	nfcd_loc key;
 	nfcd_loc value;
 };
 
+// Extracts the offset from an `nfcd_loc` item.
 #define LOC_OFFSET(loc)			((loc) >> NFCD_TYPE_BITS)
+
+// Extracts the type from an `nfcd_loc` item.
 #define LOC_TYPE(loc)			((loc) & NFCD_TYPE_MASK)
+
+// Makes an `nfcd_loc` item from object and type.
 #define MAKE_LOC(type, offset)	((type) | (offset) << NFCD_TYPE_BITS)
 
 static nfcd_loc write(struct nfcd_ConfigData **cdp, int type, void *p, int count, int zeroes);
-static struct nfcd_ObjectItem *object_item(struct nfcd_ConfigData *cd, nfcd_loc object, int i);
+static struct object_item *object_item(struct nfcd_ConfigData *cd, nfcd_loc object, int i);
 
 static nfcd_loc write(struct nfcd_ConfigData **cdp, int type, void *p, int count, int zeroes)
 {
@@ -113,6 +141,9 @@ static nfcd_loc write(struct nfcd_ConfigData **cdp, int type, void *p, int count
 	return loc;
 }
 
+// Creates a new `nfcd_ConfigData` object. The `realloc` function will be used for 
+// allocating the data. `config_size` and `stringtable_size` specify the original size
+// of the config data and the string table data. You can use 0 for a default size.
 struct nfcd_ConfigData *nfcd_make(nfcd_realloc realloc, void *ud, int config_size, int stringtable_size)
 {
 	if (!config_size)
@@ -135,45 +166,52 @@ struct nfcd_ConfigData *nfcd_make(nfcd_realloc realloc, void *ud, int config_siz
 	return cd;
 }
 
+// Returns the root item of the config data.
 nfcd_loc nfcd_root(struct nfcd_ConfigData *cd)
 {
 	return cd->root;
 }
 
+// Returns the type of the config data item `loc`.
 int nfcd_type(struct nfcd_ConfigData *cd, nfcd_loc loc)
 {
 	return LOC_TYPE(loc);
 }
 
+// Returns the numeric representation of `loc`.
 double nfcd_to_number(struct nfcd_ConfigData *cd, nfcd_loc loc)
 {
 	return *(double *)((char *)cd + LOC_OFFSET(loc));
 }
 
+// Returns the string representation of `loc`.
 const char *nfcd_to_string(struct nfcd_ConfigData *cd, nfcd_loc loc)
 {
 	return nfst_to_string(cd->string_table, LOC_OFFSET(loc));
 }
 
+// Returns the number of array items in `loc`.
 int nfcd_array_size(struct nfcd_ConfigData *cd, nfcd_loc array)
 {
-	struct nfcd_Block *arr = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(array));
+	struct block *arr = (struct block *)((char *)cd + LOC_OFFSET(array));
 	int sz = 0;
 	sz += arr->size;
 	while (arr->next_block) {
-		arr = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(arr->next_block));
+		arr = (struct block *)((char *)cd + LOC_OFFSET(arr->next_block));
 		sz += arr->size;
 	}
 	return sz;
 }
 
+// Returns the item at index `i` of the array, or `nfcd_null()` if `i` is
+// beyond the end of the array.
 nfcd_loc nfcd_array_item(struct nfcd_ConfigData *cd, nfcd_loc array, int i)
 {
 	assert(i >= 0);
-	struct nfcd_Block *arr = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(array));
+	struct block *arr = (struct block *)((char *)cd + LOC_OFFSET(array));
 	while (arr->next_block && i >= arr->size) {
 		i -= arr->size;
-		arr = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(arr->next_block));
+		arr = (struct block *)((char *)cd + LOC_OFFSET(arr->next_block));
 	}
 	if (i >= arr->size)
 		return nfcd_null();
@@ -181,95 +219,120 @@ nfcd_loc nfcd_array_item(struct nfcd_ConfigData *cd, nfcd_loc array, int i)
 	return items[i];
 }
 
+// Returns the number of key-value pairs in `loc`.
 int nfcd_object_size(struct nfcd_ConfigData *cd, nfcd_loc obj)
 {
-	struct nfcd_Block *block = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(obj));
+	struct block *block = (struct block *)((char *)cd + LOC_OFFSET(obj));
 	int sz = 0;
 	sz += block->size;
 	while (block->next_block) {
-		block = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(block->next_block));
+		block = (struct block *)((char *)cd + LOC_OFFSET(block->next_block));
 		sz += block->size;
 	}
 	return sz;
 }
 
-static struct nfcd_ObjectItem *object_item(struct nfcd_ConfigData *cd, nfcd_loc object, int i)
+// Returns the `i`th `object_item` in `loc`. Or `NULL` if `i` is beyond the
+// end of the object.
+static struct object_item *object_item(struct nfcd_ConfigData *cd, nfcd_loc object, int i)
 {
 	assert(i >= 0);
-	struct nfcd_Block *block = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(object));
+	struct block *block = (struct block *)((char *)cd + LOC_OFFSET(object));
 	while (block->next_block && i >= block->size) {
 		i -= block->size;
-		block = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(block->next_block));
+		block = (struct block *)((char *)cd + LOC_OFFSET(block->next_block));
 	}
 	if (i >= block->size)
 		return NULL;
-	struct nfcd_ObjectItem *items = (struct nfcd_ObjectItem *)(block + 1);
+	struct object_item *items = (struct object_item *)(block + 1);
 	return items + i;
 }
 
+// Returns the `i`th key as an `nfcd_loc`.
 nfcd_loc nfcd_object_keyloc(struct nfcd_ConfigData *cd, nfcd_loc object, int i)
 {
-	struct nfcd_ObjectItem *item = object_item(cd, object, i);
+	struct object_item *item = object_item(cd, object, i);
 	if (!item)
 		return nfcd_null();
 	return item->key;
 }
 
+// Returns the `i`th key as a string.
 const char *nfcd_object_key(struct nfcd_ConfigData *cd, nfcd_loc object, int i)
 {
-	struct nfcd_ObjectItem *item = object_item(cd, object, i);
+	struct object_item *item = object_item(cd, object, i);
 	if (!item)
 		return NULL;
 	return nfcd_to_string(cd, item->key);
 }
 
+// Returns the `i`th value.
 nfcd_loc nfcd_object_value(struct nfcd_ConfigData *cd, nfcd_loc object, int i)
 {
-	struct nfcd_ObjectItem *item = object_item(cd, object, i);
+	struct object_item *item = object_item(cd, object, i);
 	if (!item)
 		return nfcd_null();
 	return item->value;
 }
 
+// Looks up the item with the key `key` in `object` and returns its value.
+//
+// If there is no item with the `key`, `nfcd_null()` is returned.
+//
+// Note that the lookup uses linear search O(n) to find the entry with the
+// specified key. Objects are assumed to be "smallish" so that the extra
+// overhead for more complicated lookups (hashtables, etc) is not needed.
+// If you are dealing with an object with tens of thousands of keys, consider
+// holding your own lookup structures to be able to quickly find items in
+// the object, without the O(n) cost of `nfcd_object_lookup()`.
 nfcd_loc nfcd_object_lookup(struct nfcd_ConfigData *cd, nfcd_loc object, const char *key)
 {
 	nfcd_loc key_loc = MAKE_LOC(NFCD_TYPE_STRING, nfst_to_symbol_const(cd->string_table, key));
 
-	struct nfcd_Block *block = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(object));
+	struct block *block = (struct block *)((char *)cd + LOC_OFFSET(object));
 	while (1) {
-		struct nfcd_ObjectItem *items = (struct nfcd_ObjectItem *)(block + 1);
+		struct object_item *items = (struct object_item *)(block + 1);
 		for (int i=0; i<block->size; ++i) {
 			if (key_loc == items[i].key)
 				return items[i].value;
 		}
 		if (block->next_block == 0)
 			break;
-		block = (struct nfcd_Block *)((char *)cd + LOC_OFFSET(block->next_block));
+		block = (struct block *)((char *)cd + LOC_OFFSET(block->next_block));
 	}
 
 	return nfcd_null();
 }
 
+// Returns the `null` object. 
 nfcd_loc nfcd_null()
 {
 	return MAKE_LOC(NFCD_TYPE_NULL, 0);
 }
 
+// Returns the `false` object.
 nfcd_loc nfcd_false()
 {
 	return MAKE_LOC(NFCD_TYPE_FALSE, 0);
 }
 
+// Returns the `true` object.
 nfcd_loc nfcd_true()
 {
 	return MAKE_LOC(NFCD_TYPE_TRUE, 0);
 }
 
-nfcd_loc nfcd_add_number(struct nfcd_ConfigData **cd, double n)
+// Adds the number `n` to the config data and returns its reference.
+//
+// Note that if there is not room enough for the number, the data will
+// be reallocated and `*cd` will be modified. This is true for all
+// functions that can add data to the config data.
+nfcd_loc nfcd_add_number(struct nfcd_ConfigData **cdp, double n)
 {
-	return write(cd, NFCD_TYPE_NUMBER, &n, sizeof(n), 0);
+	return write(cdp, NFCD_TYPE_NUMBER, &n, sizeof(n), 0);
 }
 
+// Adds the string `s` to the config data nad returns its reference.
 nfcd_loc nfcd_add_string(struct nfcd_ConfigData **cdp, const char *s)
 {
 	struct nfcd_ConfigData *cd = *cdp;
@@ -287,49 +350,57 @@ nfcd_loc nfcd_add_string(struct nfcd_ConfigData **cdp, const char *s)
 	return MAKE_LOC(NFCD_TYPE_STRING, sym);
 }
 
+// Adds a new array, preallocated to the specified size to the config data
+// and returns its reference.
 nfcd_loc nfcd_add_array(struct nfcd_ConfigData **cdp, int allocated_size)
 {
-	struct nfcd_Block a = {0};
+	struct block a = {0};
 	a.allocated_size = allocated_size;
 	return write(cdp, NFCD_TYPE_ARRAY, &a, sizeof(a), allocated_size * sizeof(nfcd_loc));
 }
 
+// Adds a new object to the config adta and returns its reference.
 nfcd_loc nfcd_add_object(struct nfcd_ConfigData **cdp, int allocated_size)
 {
-	struct nfcd_Block a = {0};
+	struct block a = {0};
 	a.allocated_size = allocated_size;
-	return write(cdp, NFCD_TYPE_OBJECT, &a, sizeof(a), allocated_size * sizeof(struct nfcd_ObjectItem));
+	return write(cdp, NFCD_TYPE_OBJECT, &a, sizeof(a), allocated_size * sizeof(struct object_item));
 }
 
+// Sets the root object of the config data.
 void nfcd_set_root(struct nfcd_ConfigData *cd, nfcd_loc loc)
 {
 	cd->root = loc;
 }
 
+// Pushes `item` to the end of the `array`.
 void nfcd_push(struct nfcd_ConfigData **cdp, nfcd_loc array, nfcd_loc item)
 {
-	struct nfcd_Block *arr = (struct nfcd_Block *)((char *)*cdp + LOC_OFFSET(array));
+	struct block *arr = (struct block *)((char *)*cdp + LOC_OFFSET(array));
 	while (arr->size == arr->allocated_size) {
 		if (arr->next_block == 0)
 			arr->next_block = nfcd_add_array(cdp, arr->allocated_size*2);
-		arr = (struct nfcd_Block *)((char *)*cdp + LOC_OFFSET(arr->next_block));
+		arr = (struct block *)((char *)*cdp + LOC_OFFSET(arr->next_block));
 	}
 	nfcd_loc *items = (nfcd_loc *)(arr + 1);
 	items[arr->size] = item;
 	++arr->size;
 }
 
-void nfcd_set(struct nfcd_ConfigData **cdp, nfcd_loc object, const char *key_str, nfcd_loc value)
+// Sets the `key` to the `value` in the `object`.
+void nfcd_set(struct nfcd_ConfigData **cdp, nfcd_loc object, const char *key, nfcd_loc value)
 {
-	nfcd_loc key = nfcd_add_string(cdp, key_str);
-	nfcd_set_loc(cdp, object, key, value);
+	nfcd_loc key_loc = nfcd_add_string(cdp, key);
+	nfcd_set_loc(cdp, object, key_loc, value);
 }
 
+// Sets the `key` to the `value` in the `object`. Note that only string
+// keys are allowed.
 void nfcd_set_loc(struct nfcd_ConfigData **cdp, nfcd_loc object, nfcd_loc key, nfcd_loc value)
 {
-	struct nfcd_Block *block = (struct nfcd_Block *)((char *)*cdp + LOC_OFFSET(object));
+	struct block *block = (struct block *)((char *)*cdp + LOC_OFFSET(object));
 	while (1) {
-		struct nfcd_ObjectItem *items = (struct nfcd_ObjectItem *)(block + 1);
+		struct object_item *items = (struct object_item *)(block + 1);
 		for (int i=0; i<block->size; ++i) {
 			if (items[i].key == key) {
 				items[i].value = value;
@@ -340,15 +411,16 @@ void nfcd_set_loc(struct nfcd_ConfigData **cdp, nfcd_loc object, nfcd_loc key, n
 			break;
 		if (block->next_block == 0)
 			block->next_block = nfcd_add_object(cdp, block->allocated_size*2);
-		block = (struct nfcd_Block *)((char *)*cdp + LOC_OFFSET(block->next_block));
+		block = (struct block *)((char *)*cdp + LOC_OFFSET(block->next_block));
 	}
 
-	struct nfcd_ObjectItem *items = (struct nfcd_ObjectItem *)(block + 1);
+	struct object_item *items = (struct object_item *)(block + 1);
 	items[block->size].key = key;
 	items[block->size].value = value;
 	++block->size;
 }
 
+// Returns the allocateor and the user data of the config data.
 nfcd_realloc nfcd_allocator(struct nfcd_ConfigData *cd, void **user_data)
 {
 	*user_data = cd->realloc_user_data;
@@ -406,20 +478,6 @@ nfcd_realloc nfcd_allocator(struct nfcd_ConfigData *cd, void **user_data)
 		assert(nfcd_type(cd, nfcd_object_lookup(cd, obj, "age")) == NFCD_TYPE_NUMBER);
 		assert(nfcd_to_number(cd, nfcd_object_lookup(cd, obj, "age")) == 41);
 		assert(nfcd_type(cd, nfcd_object_lookup(cd, obj, "title")) == NFCD_TYPE_NULL);
-
-		/*
-
-		nfcd_loc people = nfcd_make_array(cd, 10);
-
-		nfcd_loc niklas = nfcd_make_object(cd, 5);
-
-		// C Generics
-		nfcd_set_key(cd, niklas, "first", "Niklas");
-		nfcd_set_key(cd, niklas, "last", "Frykholm");
-		nfcd_set_key(cd, niklas, "age", 41);
-
-		nfcd_append(cd, people, niklas);
-		*/
 	}
 
 #endif
